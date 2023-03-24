08a08e6c6fb41eb128642d0fffb5a7c1205f3074
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-03-24 11:12:16 -0700

    Adds test for multiple data types in duplicate detection method for letters

--- Ruby/find_duplicates.rb
+++ Ruby/find_duplicates.rb
@@ -24,23 +24,32 @@ end
 
 array_three = ["a", "b", "c", "d", "e", "f", "g"]
 array_four = ["a", "b", "c", "a", "e", "f", "g"]
+array_five = [1, "a,", 2.0, 3, "Hello", 7, 2134]
+array_six = [1, "a,", 2.0, 3, "Hello", "Hello", 7]
 
 def find_duplicate_letters(array)
   letter_hash = {}
+  steps = 0
   for i in array
     letter_hash[i] = 0
+    steps += 1
   end
   for i in array
     letter_hash[i] += 1
     p letter_hash
+    steps += 1
     if letter_hash[i] > 1
       p "true"
+      p "Steps: #{steps}"
       return true
     end
   end
   p "false"
+  p "Steps: #{steps}"
   return false
 end
 
-find_duplicate_letters(array_three)
-find_duplicate_letters(array_four)
+# find_duplicate_letters(array_three)
+# find_duplicate_letters(array_four)
+find_duplicate_letters(array_five)
+find_duplicate_letters(array_six)
